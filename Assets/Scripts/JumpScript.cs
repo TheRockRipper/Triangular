e57fd0b9c7bdbb968782bafcb87887193d212446
TheRockRipper <krishna.rohitx@gmail.com>
2017-03-06 11:26:32 +0530

    Trying to rotate the line renderer
    Do I need to use sine or cosine math functions to rotate the second
    point of the line renderer?

--- Assets/Scripts/JumpScript.cs
+++ Assets/Scripts/JumpScript.cs
@@ -5,15 +5,19 @@ public class JumpScript : MonoBehaviour {
 
 	SpriteRenderer sr;
 	LineRenderer lr;
+
 	// Use this for initialization
 	void Start () {
-		sr = GetComponent<SpriteRenderer> ();
+		//sr = GetComponent<SpriteRenderer> ();
 		lr = GetComponent<LineRenderer> ();
-		 Debug.Log (sr.bounds.center.x);
+		//lr.GetPosition ();
+		//Debug.Log (lr.GetPosition(1));
+ //			 Debug.Log (sr.bounds.center.x);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+		//Vector2 mouseposition = new Vector2 (Input.mousePosition.x, Input.mousePosition.y);
+		Debug.Log(lr.GetPosition(1));
 	}
 }
